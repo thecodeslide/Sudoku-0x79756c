8cf438c94b3bc62f00ea9ba41910d6162b7b175f
thecodeslide <45506833+thecodeslide@users.noreply.github.com>
2024-07-01 07:30:57 -0300

    Update LibrarySet.sol

--- LibrarySet.sol
+++ LibrarySet.sol
@@ -19,9 +19,9 @@ library SetSudokuLib {
       mstore(0x20, set.slot)
       result := sload(keccak256(0, 0x40))
    
-      let val := mod(cellValue, 0xff) // bytes1
+      let val := mod(cellValue, 0xff)
       let position := shl(mul(val, 8), val)
-      position := and(position, mul(exp(0x100, val), 0xff)) // clean the value, ensure the right offset(in this casse) and size
+      position := and(position, mul(exp(0x100, val), 0xff))
       if and(result, position) {
         result := hex"01"
       }
@@ -35,11 +35,11 @@ library SetSudokuLib {
       mstore(0, key)
       mstore(0x20, set.slot)
  
-      let shifted := mod( cellValue, 0xff ) // 0x000... x
-      let position := shl( mul( shifted, 8 ), shifted) // 8 bits .. 1 byte
+      let shifted := mod( cellValue, 0xff )
+      let position := shl( mul( shifted, 8 ), shifted) 
       let result :=  and( mul( exp( 0x100, shifted ) , 0xff ) , position )
-      let mask := not(mul(exp(0x0100, shifted), 0xff))// mask to clear offset for insert
-      result := or(and(mask, sload(keccak256(0, 0x40))), result) // putting mask here results in dup operation
+      let mask := not(mul(exp(0x0100, shifted), 0xff))
+      result := or(and(mask, sload(keccak256(0, 0x40))), result)
       sstore(keccak256(0, 0x40), result)
     }
   }
@@ -60,7 +60,7 @@ library SetSudokuLib {
 
         if or(_hash, sload(hash)) {
           let frame := mload(0x40)
-          mstore(frame, hex"4e487b71") //Panic
+          mstore(frame, hex"4e487b71") 
           mstore(add(frame, 0x4), 1)
           revert(frame, 0x24)
         }
