05a2b3e86bf391803ac1563a8d89c8f9111be4a7
thecodeslide <45506833+thecodeslide@users.noreply.github.com>
2024-07-11 03:20:39 -0300

    add functions

--- SudokuCheckerCaller.sol
+++ SudokuCheckerCaller.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.17;
 
 // interface solidity contract will use to call Yul contract
-interface ISudokuYulExStor {
+interface ISudokuYulBuildSpace {
     function fun_isValidBlocks(uint256[9][9] calldata board) external returns (uint256);
+    function fun_isValidRows(uint256[9][9] calldata board) external returns (uint256);
+    function fun_isValidCols(uint256[9][9] calldata board) external returns (uint256);
+    function fun_isValid(uint256[9][9] calldata board) external returns (uint256);
     function get_lib() external returns (address);
     function set_lib(address addr) external returns (uint);
 }
-
 contract CallSudokuYulExStor {
     ISudokuYulExStor public target;
 
@@ -15,15 +17,27 @@ contract CallSudokuYulExStor {
         target = _target;
     }
     
+    function isValid(uint256[9][9] calldata board) external returns (uint256) {
+        return target.fun_isValid(board);
+    }
+    
     function validBlock(uint256[9][9] calldata board) external returns (uint256) {
-        return target.fun_isValidBlocks(board); // cf608056
+        return target.fun_isValidBlocks(board);
+    }
+
+    function validCols(uint256[9][9] calldata board) external returns (uint256) {
+        return target.fun_isValidCols(board);
+    }
+
+    function validRows(uint256[9][9] calldata board) external returns (uint256) {
+        return target.fun_isValidRows(board);
     }
 
     function getLibAddr() public  returns (address) {
         return target.get_lib();
     }
 
-    function setLib(address addr) external returns (uint) { // 245cba0a
+    function setLib(address addr) external returns (uint) {
         return target.set_lib(addr);
     }
 }
